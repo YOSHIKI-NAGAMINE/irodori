5141047fce601696a58a5cf9af6dc2858c01fac4
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-22 16:49:12 +0900

    しおり情報保存機能実装

--- app/controllers/sioris_controller.rb
+++ app/controllers/sioris_controller.rb
@@ -3,5 +3,15 @@ class SiorisController < ApplicationController
   end
 
   def new
+    @siori = Siori.new
+  end
+
+  def create
+    @siori = Siori.create(siori_params)
+  end
+
+  private
+  def siori_params
+    params.require(:siori).permit(:title)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_21_125318) do
+ActiveRecord::Schema.define(version: 2023_03_22_065810) do
+
+  create_table "sioris", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
