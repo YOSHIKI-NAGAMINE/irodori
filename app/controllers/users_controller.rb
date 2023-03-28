b8ff952ab78f6463bd95c57bc02184ece7ddf645
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-28 11:45:34 +0900

    目標名表示機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
     @siori = Siori.all
+    @achevements = Achevement.where(user_id: current_user.id)
   end
 
 
