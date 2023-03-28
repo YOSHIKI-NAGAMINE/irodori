5cb56503afff5de90d392b7d265e9111b54a24f0
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-28 14:48:31 +0900

    目標画像、タイトル表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
     @siori = Siori.all
-    @achevements = Achevement.where(user_id: current_user.id)
+    @achevements = Achevement.all.order("goal_number")
   end
 
 
